efcfc7f39b13366c17a8297c8a8ed8023f465a66
Fraz Ahsan <fraz.ahsan17@gmail.com>
2017-02-22 16:46:45 +0500

    Login with facebook added

---
+++ config/application.rb
@@ -0,0 +1,20 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module SuggestMe
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config_keys = File.join(Rails.root, 'config', 'config.yml')
+    CONFIG = HashWithIndifferentAccess.new(YAML::load(IO.read(config_keys)))[Rails.env]
+    CONFIG.each do |k,v|
+      ENV[k.upcase] ||= v
+    end
+  end
+end
