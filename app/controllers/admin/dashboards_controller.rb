a6459ce3ae7e66bf91389c17e3d3b291992c1770
Fraz Ahsan <fraz.ahsan17@gmail.com>
2018-03-06 23:10:50 +0500

    Admin Changes

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,53 +1,53 @@
 class Admin::DashboardsController < Admin::AppController
-	skip_before_action :admin_restricted, only: :profile
+  skip_before_action :admin_restricted, only: :profile
   # layout "application", only: [:messages]
-	def dashboard
-		@search = UserSearch.new(params)
+  def dashboard
+    @search = UserSearch.new(params)
     @users = @search.result.where.not(:role_cd => 0).paginate(page: params[:page], per_page: 5)
-	end
+  end
 
-	def logs
-		@logs = Log.where(log_type: "message").order("created_at desc").last(100)
-	end
+  def logs
+    @logs = Log.where(log_type: "message").order("created_at desc").last(100)
+  end
 
-	def messages
-		@messages = Message.order("id desc").first(20)
-		@user = current_user
+  def messages
+    @messages = Message.order("id desc").first(20)
+    @user = current_user
     render layout: "application"
-	end
+  end
 
-	def change_category
-		@message = Message.find(params[:message_id])
-		category = AvailableCategory.find_by(name: params[:category])
-		@message.available_category = category
-		@message.save
-		respond_to do |format|
+  def change_category
+    @message = Message.find(params[:message_id])
+    category = AvailableCategory.find_by(name: params[:category])
+    @message.available_category = category
+    @message.save
+    respond_to do |format|
       format.js
     end
-	end
+  end
 
-	def refresh_messages
-		@messages = Message.order("id desc").first(20)
-		@user = current_user
-		respond_to do |format|
+  def refresh_messages
+    @messages = Message.order("id desc").first(20)
+    @user = current_user
+    respond_to do |format|
       format.js
     end
-	end
+  end
 
-	def change_role
-		@user = User.find(params[:id])
-		if @user.role == :user
-			role = :manager
-		else
-			role = :user	
-		end
-		@user.update_attributes(:role => role)
-		respond_to do |format|
-			format.js 
-		end
-	end
+  def change_role
+    @user = User.find(params[:id])
+    if @user.role == :user
+      role = :manager
+    else
+      role = :user
+    end
+    @user.update_attributes(:role => role)
+    respond_to do |format|
+      format.js
+    end
+  end
 
-	def import
+  def import
     AbuseFilter.import(params[:file])
     redirect_to request.referer, notice: "File imported."
   end
@@ -55,45 +55,45 @@ class Admin::DashboardsController < Admin::AppController
   def import_file
   end
 
-	def block_unblock
-		user = User.find_by(:id => params[:user_id])
-		if user.blocked
-			block = false
-		else
-			block = true
-		end	
-		user.update_attributes(:blocked => block)
-		redirect_to admin_dashboard_path, notice: block ? "Successfully Blocked" : "Successfully Unblocked!"
-	end
+  def block_unblock
+    user = User.find_by(:id => params[:user_id])
+    if user.blocked
+      block = false
+    else
+      block = true
+    end
+    user.update_attributes(:blocked => block)
+    redirect_to admin_dashboard_path, notice: block ? "Successfully Blocked" : "Successfully Unblocked!"
+  end
 
-	def broadcast_message
-	end
+  def broadcast_message
+  end
 
-	def send_message
-		@message = Message.create(content: params[:message], sender_id: current_user.id)
-		User.all.each do |user|
-  		user.chat_room.messages << @message
+  def send_message
+    @message = Message.create(content: params[:message], sender_id: current_user.id)
+    User.all.each do |user|
+      user.chat_room.messages << @message
       BroadcastAllJob.perform_now(user, @message, user.chat_room.id)
-  	end
-  	redirect_to request.referer
-	end
+    end
+    redirect_to request.referer
+  end
 
-	def reported_messages
-		@messages = ReportedMessage.get_messages
-	end
+  def reported_messages
+    @messages = ReportedMessage.get_messages
+  end
 
-	def delete_message
-		if params[:id].present?
-			msg = Message.find_by_id(params[:id])
-			msg.destroy if msg.present?
-		end
-		redirect_to request.referer, notice: "Successfully Deleted!"
-	end
+  def delete_message
+    if params[:id].present?
+      msg = Message.find_by_id(params[:id])
+      msg.destroy if msg.present?
+    end
+    redirect_to request.referer, notice: "Successfully Deleted!"
+  end
 
-	def profile
-		@user = User.find(params[:id])
-		respond_to do |format|
-			format.js 
-		end
-	end
+  def profile
+    @user = User.find(params[:id])
+    respond_to do |format|
+      format.js
+    end
+  end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,9 @@
 require 'fuzzy_match'
+
 class MessagesController < ApplicationController
-	before_action :check_admin, except: [:post_reply]
+  before_action :check_admin, except: [:post_reply]
   before_action :authenticate_user!
-	before_action :check_profile, :if => proc {|c| !request.xhr?}
+  before_action :check_profile, :if => proc { |c| !request.xhr? }
 
   def index
     @messages = Message.where("content ilike ? and visible = ?", "%#{params[:term]}%", true)
@@ -15,11 +16,11 @@ class MessagesController < ApplicationController
 
   def post_message
     params.permit!
-  	@message = Message.create(content: params[:message], sender_id: current_user.id)
+    @message = Message.create(content: params[:message], sender_id: current_user.id)
     @message.sender.add_loyalty_score(10) if !@message.content.include?("*")
     log = Log.create(content: params[:message], sender_id: current_user.id, log_type: "message", message_id: @message.id)
-    current_user.chat_room.messages << @message  
-  	MessageBroadcastJob.perform_now(current_user, @message, current_user.chat_room.id)
+    current_user.chat_room.messages << @message
+    MessageBroadcastJob.perform_now(current_user, @message, current_user.chat_room.id)
     LogBroadcastJob.set(wait: 10.seconds).perform_later(log)
     UpdateRepliesJob.set(wait: 60.minutes).perform_later(current_user, @message, current_user.chat_room.id, true)
     if current_user.matchers.present? && current_user.matchers.count >= 10
@@ -65,9 +66,9 @@ class MessagesController < ApplicationController
       category.save
       result = true
     else
-      result = false  
+      result = false
     end
-    render json: { success: true }
+    render json: {success: true}
   end
 
   def report
@@ -85,7 +86,7 @@ class MessagesController < ApplicationController
         message.sender.add_loyalty_score(20)
         message.sender.minus_flag_point
         message.minus_flag_point
-      else  
+      else
         report = ReportedMessage.new
         report.message_id = params[:message_id] if params[:type].blank?
         report.reply_id = params[:message_id] if params[:type].present?
@@ -98,7 +99,7 @@ class MessagesController < ApplicationController
         message.sender.minus_loyalty_score(20)
         message.sender.add_flag_point
         message.add_flag_point
-      end  
+      end
     end
     @type = params[:type]
     @message = message
@@ -140,13 +141,15 @@ class MessagesController < ApplicationController
   end
 
   private
+
   def check_profile
-  	if user_signed_in? && current_user.complete_profile == false
-  		redirect_to profile_path
-  	else
-  		return true
-  	end 
+    if user_signed_in? && current_user.complete_profile == false
+      redirect_to profile_path
+    else
+      return true
+    end
   end
+
   def check_admin
     if user_signed_in? && (current_user.role == :admin || current_user.role == :manager)
       redirect_to admin_dashboard_path
